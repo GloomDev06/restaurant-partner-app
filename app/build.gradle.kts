914ef366fd33544747938bf4c2d865b3c6cfe429
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-09-06 03:40:28 +0530

    Add New Dish to Menu Backend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,6 +62,6 @@ dependencies {
     implementation (libs.navigation.fragment.ktx)
     implementation (libs.navigation.ui.ktx)
 
-
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
 }

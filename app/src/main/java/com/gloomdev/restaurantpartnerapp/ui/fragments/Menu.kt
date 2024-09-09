a596a0724aabb86991e648dd02b1ada01f89b992
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-09-09 18:37:32 +0530

    Navigation and UI fixes for Fragment

--- app/src/main/java/com/gloomdev/restaurantpartnerapp/ui/fragments/Menu.kt
+++ app/src/main/java/com/gloomdev/restaurantpartnerapp/ui/fragments/Menu.kt
@@ -1,60 +1,74 @@
 package com.gloomdev.restaurantpartnerapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gloomdev.restaurantpartnerapp.R
+import com.gloomdev.restaurantpartnerapp.adapters.MenuItemAdapter
+import com.gloomdev.restaurantpartnerapp.databinding.FragmentHomeBinding
+import com.gloomdev.restaurantpartnerapp.databinding.FragmentMenuBinding
+import com.gloomdev.restaurantpartnerapp.models.AllMenu
+import com.gloomdev.restaurantpartnerapp.ui.activities.AddMenuActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Menu.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Menu : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private var menuItems : ArrayList<AllMenu> = ArrayList()
+    private lateinit var binding: FragmentMenuBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+        binding = FragmentMenuBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Menu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Menu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addMenu.setOnClickListener {
+            val intent = Intent(requireContext(), AddMenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        databaseReference = FirebaseDatabase.getInstance().reference
+        retrieveMenuItem()
+    }
+
+    private fun retrieveMenuItem() {
+        database = FirebaseDatabase.getInstance()
+        val foodRef: DatabaseReference = database.reference.child("menu")
+        foodRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                menuItems.clear()
+                for (foodSnapshot in snapshot.children) {
+                    val menuItem = foodSnapshot.getValue(AllMenu::class.java)
+                    menuItem?.let {
+                        menuItems.add(it)
+                    }
                 }
+                setAdapter()
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("DatabaseError", "onCancelled: ${error.message}")
+            }
+        })
+    }
+
+    private fun setAdapter() {
+        val adapter = MenuItemAdapter(requireContext(), menuItems, databaseReference)
+        binding.menuRV.layoutManager = LinearLayoutManager(requireContext())
+        binding.menuRV.adapter = adapter
     }
 }

a1438cfe02b5dcb2a5db4eb3331c34cb31bb0f66
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-09-21 19:33:15 +0530

    added restaurant id to menu items

--- app/src/main/java/com/gloomdev/restaurantpartnerapp/models/AllMenu.kt
+++ app/src/main/java/com/gloomdev/restaurantpartnerapp/models/AllMenu.kt
@@ -5,5 +5,6 @@ data class AllMenu(
     val foodName: String? = null,
     val foodPrice: String? = null,
     val foodDescription: String? = null,
-    val foodImage: String? = null
+    val foodImage: String? = null,
+    val IdOfRestaurant: String? = null
 )
--- app/src/main/java/com/gloomdev/restaurantpartnerapp/ui/activities/AddMenuActivity.kt
+++ app/src/main/java/com/gloomdev/restaurantpartnerapp/ui/activities/AddMenuActivity.kt
@@ -79,7 +79,7 @@ class AddMenuActivity : AppCompatActivity() {
 
             uploadTask.addOnSuccessListener {
                 imageRef.downloadUrl.addOnSuccessListener { downloadUrl ->
-                    val newMenu = AllMenu(newItemKey, foodName, foodPrice, foodDescription, downloadUrl.toString())
+                    val newMenu = AllMenu(newItemKey, foodName, foodPrice, foodDescription, downloadUrl.toString(), userId)
                     newItemKey?.let { key->
                         menuRef.child(key).setValue(newMenu).addOnSuccessListener {
                             Toast.makeText(this, "Data Upload Successful", Toast.LENGTH_SHORT).show()
